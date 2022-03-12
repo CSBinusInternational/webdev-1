b3d640ac5086c10ea9b0acb322ca70bd549d551a
kalyanrohan <sri.rohan@binus.ac.id>
2022-03-12 10:54:54 +0700

    login forum

---
+++ forum/login/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.scss';
+import React from 'react';
+import { Login } from "./components/login/index";
+
+function App(){
+  return (
+    <div className="App">
+    <Login />
+    </div>
+  )
+}
+
+export default App;
